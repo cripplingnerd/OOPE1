3df6aa2d131e474facb3fc72e568d73576646308
Sankhaja Hapukotuwa <sankhe1500@gmail.com>
2020-11-25 03:11:30 +0000

    Add method to calculate the total area of the composite figure

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@ using namespace std;
 #include "Triangle.h"
 
 int main() {
+  double totArea;
+    
+  
   Rectangle r1(7, 14);
   
   Triangle t1(12, 10), t2(18, 18);
@@ -17,7 +20,10 @@ int main() {
   
   t2.area();
 
-  cout << r1.area() << endl;
-  cout << t1.area() << endl;
-  cout << t2.area() << endl;
+  totArea = (r1.area() + t1.area() + t2.area());
+
+  cout << "The area of the rectangle: " << r1.area() << endl;
+  cout << "The area of the triangle t1: " << t1.area() << endl;
+  cout <<"The area of the triangle t2: " << t2.area() << endl;
+  cout << "The total area of the composite figure: " << totArea << endl;
 }
